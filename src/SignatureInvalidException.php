85f5984b8ab022f659795c89a1471303dde3d109
MMauro94 <molin.mauro@gmail.com>
2017-04-28 10:48:47 +0200

    Update SignatureInvalidException.php

--- src/SignatureInvalidException.php
+++ src/SignatureInvalidException.php
@@ -3,5 +3,14 @@ namespace Firebase\JWT;
 
 class SignatureInvalidException extends \UnexpectedValueException
 {
-
+  
+	private $payload;
+  
+	public function setPayload($payload){
+		$this->payload = $payload;
+	}
+  
+	public function getPayload(){
+		return $this->payload;
+	}
 }
