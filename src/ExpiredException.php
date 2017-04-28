007148a5673965a905370a3512aafe810f4dea4a
MMauro94 <molin.mauro@gmail.com>
2017-04-28 10:43:45 +0200

    Added payload

--- src/ExpiredException.php
+++ src/ExpiredException.php
@@ -3,5 +3,13 @@ namespace Firebase\JWT;
 
 class ExpiredException extends \UnexpectedValueException
 {
+	private $payload;
 
+	public function __construct($payload = null){
+		$this->payload = $payload;
+	}
+
+	public function getPayload(){
+		return $this->payload;
+	}
 }
