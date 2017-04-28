a0d2f68e62250589c32aaf17939a7d0e07745ff5
MMauro94 <molin.mauro@gmail.com>
2017-04-28 10:45:41 +0200

    Update BeforeValidException.php

--- src/BeforeValidException.php
+++ src/BeforeValidException.php
@@ -4,9 +4,11 @@ namespace Firebase\JWT;
 class BeforeValidException extends \UnexpectedValueException
 {
 	private $payload;
-	public function __construct($payload = null){
+	
+	public function setPayload($payload){
 		$this->payload = $payload;
 	}
+	
 	public function getPayload(){
 		return $this->payload;
 	}
