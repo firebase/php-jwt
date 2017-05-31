839d4a83d51dfc0035bf901afa55a7873dfbf2b7
chibana <chibana@camobile.com>
2017-05-31 17:02:07 +0900

    ignore json_encode error

--- src/JWT.php
+++ src/JWT.php
@@ -294,7 +294,7 @@ class JWT
      */
     public static function jsonEncode($input)
     {
-        $json = json_encode($input);
+        $json = @json_encode($input);
         if (function_exists('json_last_error') && $errno = json_last_error()) {
             static::handleJsonError($errno);
         } elseif ($json === 'null' && $input !== null) {
