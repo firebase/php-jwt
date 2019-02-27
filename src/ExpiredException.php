eb1f3c6418a7aba4f500d0b7d1b6fe64e8ca2b0b
Filipe Voges <filipe@emsventura.com>
2019-02-27 11:17:39 -0300

    Added Token expiration time when thrown exception.

--- src/ExpiredException.php
+++ src/ExpiredException.php
@@ -4,4 +4,52 @@ namespace Firebase\JWT;
 class ExpiredException extends \UnexpectedValueException
 {
 
+    /**
+     * @var \DateInterval
+     */
+    protected $timeExpired;
+
+    /**
+     * Construct Class
+     *
+     * @param string $message
+     * @param \DateInterval $timeExpired
+     * @return void
+     *
+     * @author Filipe Voges <filipe.vogesh@gmail.com>
+     * @since 2019-02-27
+     * @version PHP-JWT v5.0.0
+     */
+    public function __construct(string $message, \DateInterval $timeExpired){
+        parent::__construct($message);
+
+        $this->timeExpired = $timeExpired;
+    }
+
+    /**
+     * Get timeExpired
+     *
+     * @return \DateInterval
+     *
+     * @author Filipe Voges <filipe.vogesh@gmail.com>
+     * @since 2019-02-27
+     * @version PHP-JWT v5.0.0
+     */
+    public function getTimeExpired(){
+        return $this->timeExpired;
+    }
+
+    /**
+     * Returns the time the token is expired in a specific format
+     *
+     * @param string $format
+     * @return string
+     *
+     * @author Filipe Voges <filipe.vogesh@gmail.com>
+     * @since 2019-02-27
+     * @version PHP-JWT v5.0.0
+     */
+    public function getTimeExpiredFormat(string $format = 'Y-m-d H:i:s'){
+        return $this->timeExpired->format($format);
+    }
 }
