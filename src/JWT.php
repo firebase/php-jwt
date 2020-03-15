d3903c31dce1fafddf692a7c74d51ea2c89f2727
Phil Davis <phil@jankaritech.com>
2020-03-15 20:33:13 +0545

    Fix PHP doc - encode payload can be string

--- src/JWT.php
+++ src/JWT.php
@@ -150,13 +150,13 @@ class JWT
     /**
      * Converts and signs a PHP object or array into a JWT string.
      *
-     * @param object|array  $payload    PHP object or array
-     * @param string        $key        The secret key.
-     *                                  If the algorithm used is asymmetric, this is the private key
-     * @param string        $alg        The signing algorithm.
-     *                                  Supported algorithms are 'ES256', 'HS256', 'HS384', 'HS512', 'RS256', 'RS384', and 'RS512'
-     * @param mixed         $keyId
-     * @param array         $head       An array with header elements to attach
+     * @param string|object|array   $payload    PHP object or array
+     * @param string                $key        The secret key.
+     *                                          If the algorithm used is asymmetric, this is the private key
+     * @param string                $alg        The signing algorithm.
+     *                                          Supported algorithms are 'ES256', 'HS256', 'HS384', 'HS512', 'RS256', 'RS384', and 'RS512'
+     * @param mixed                 $keyId
+     * @param array                 $head       An array with header elements to attach
      *
      * @return string A signed JWT
      *
