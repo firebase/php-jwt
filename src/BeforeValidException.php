a2d3bb73de63d4745d62574e560920c64eb07c5e
nicumicle <20170987+nicumicleI@users.noreply.github.com>
2022-09-21 08:01:34 +0200

    Add public to const in BeforeValidException

--- src/BeforeValidException.php
+++ src/BeforeValidException.php
@@ -5,5 +5,5 @@ namespace Firebase\JWT;
 class BeforeValidException extends \UnexpectedValueException
 {
     public const NBF_PRIOR_TO_DATE = 1;
-    const IAT_PRIOR_TO_DATE = 2;
+    public const IAT_PRIOR_TO_DATE = 2;
 }
